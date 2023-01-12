754e892f979d722d3e1db527b011974dca3fe68c
Matt Hampton <matthew.j.hampton@gmail.com>
2023-01-12 12:39:50 +0200

    Getting started on the parsing

--- holdemup.py
+++ holdemup.py
@@ -1,3 +1,39 @@
+from enum import Enum
+from dataclasses import dataclass
+
+Suit = Enum('Suit', ['c', 'd', 'h', 's' ])
+Face = Enum('Face', ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'])
+FACE_SEQUENCE = dict((f, i+2) for i, f in enumerate(Face))
+
+
+@dataclass
+class Card:
+    suit: Suit
+    face: Face
+
+class ParseError(Exception):
+    pass
+
+def raise_parse_error(line_nr, line, message):
+    if line_nr is None:
+        raise ParseError("Parsing error in line [%s]: '%s' is not a valid card" % (line, message))
+    else:
+        raise ParseError("Parsing error at line %d [%s]: '%s' is not a valid card" % (line_nr, line, message))
+
+def parse_hand(hand_text, line_nr=None):
+    hand = []
+    for c in filter(None, hand_text.split()):
+        if len(c) != 2:
+            raise_parse_error(line_nr, hand_text, "'%s' is not a valid card" % c)
+    return hand
+
+def parse_hands(hands_text):
+    hands = []
+    for line_nr, line in enumerate(hands_text.split('\n')):
+        if not line.strip():
+            continue
+        hands.append(parse_hand(line, line_nr=line_nr))
+    return hands
 
 
 def main():
--- test_holdemup.py
+++ test_holdemup.py
@@ -1,10 +1,25 @@
 import unittest 
-from holdemup import main
+from holdemup import parse_hands, parse_hand, ParseError
 
-class TestStringMethods(unittest.TestCase):
+class TestHoldemUp(unittest.TestCase):
 
-    def test_main(self):
-        main()
+    def test_parse_hands_documented_example(self):
+        hands = parse_hands("""
+            Kc 9s Ks Kd 9d 3c 6d
+            9c Ah Ks Kd 9d 3c 6d
+            Ac Qc Ks Kd 9d 3c
+            9h 5s
+            4d 2d Ks Kd 9d 3c 6d
+            7s Ts Ks Kd 9d        
+        """)
+        self.assertEqual(len(hands), 6)
+
+    def test_parse_hand_valid(self):
+        hand = parse_hand("Kc 9s Ks Kd 9d 3c 6d")
+        self.assertEqual(len(hand), 7)
+
+    def test_parse_hand_invalid_card_len(self):
+        self.assertRaises(ParseError, parse_hand, "Kc 9ss Ks Kd 9d 3c 6d")
 
 if __name__ == '__main__':
     unittest.main()
